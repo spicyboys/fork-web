b3f56b60bc8baaf96af61cbdbf7cb2be2859543b
Robert Lynch <robertlynch2012@gmail.com>
2021-03-14 13:25:43 -0500

    Change project name

--- src/NavBar.svelte
+++ src/NavBar.svelte
@@ -8,7 +8,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="/">
-      <p class="title">Recipe Rolodex</p>
+      <p class="title">Fork</p>
     </a>
   </div>
 
