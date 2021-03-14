726cb71a62e2a37de59493f348ea64bbbd01a642
Robert Lynch <robertlynch2012@gmail.com>
2021-03-14 16:35:59 -0500

    Add basic text to splash

--- src/pages/Splash.svelte
+++ src/pages/Splash.svelte
@@ -1,6 +1,6 @@
 <section class="hero is-primary">
   <div class="hero-body">
-    <p class="title">Primary hero</p>
-    <p class="subtitle">Primary subtitle</p>
+    <p class="title">Welcome to Fork!</p>
+    <p class="subtitle">It's like git for recipes, get it.</p>
   </div>
 </section>
