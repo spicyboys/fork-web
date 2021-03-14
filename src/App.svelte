eda8a5816fbdb0e9f32038e74ea2b5d551dd43cc
Robert Lynch <robertlynch2012@gmail.com>
2021-03-13 19:16:32 -0600

    Add Bulma and basic NavBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,14 @@
 <script lang="ts">
-	export let name: string;
+  import NavBar from "./NavBar.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <NavBar />
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"
+  />
+</svelte:head>
---
+++ src/NavBar.svelte
@@ -0,0 +1,20 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="/">
+      <p class="title">Recipe Rolodex</p>
+    </a>
+  </div>
+
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <button class="button is-primary">
+            <strong>Sign up</strong>
+          </button>
+          <button class="button is-light">Log in</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
