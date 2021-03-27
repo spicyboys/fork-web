daae6b4416817c81d45f869cff6186ac05abd7f0
Robert Lynch <robertlynch2012@gmail.com>
2021-03-27 15:12:09 -0500

    Add basic page layout

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import 'carbon-components-svelte/css/all.css';
+	import { auth } from '$lib/firebase';
+	import { user as userContext } from '$lib/contexts';
+	import { authState } from 'rxfire/auth';
+	import { setContext } from 'svelte';
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+		HeaderNavMenu,
+		Content,
+	} from 'carbon-components-svelte';
+
+	let user = authState(auth);
+	setContext(userContext, {
+		getUser: () => $user
+	});
+</script>
+
+<Header company="Fork">
+	{#if $user}
+		<HeaderNav>
+			<HeaderNavItem href="/" text="Link 1" />
+			<HeaderNavItem href="/" text="Link 2" />
+			<HeaderNavItem href="/" text="Link 3" />
+			<HeaderNavMenu text="Menu">
+				<HeaderNavItem href="/" text="Link 1" />
+				<HeaderNavItem href="/" text="Link 2" />
+				<HeaderNavItem href="/" text="Link 3" />
+			</HeaderNavMenu>
+			<HeaderNavItem href="/" text="Link 4" />
+		</HeaderNav>
+	{/if}
+</Header>
+
+<Content>
+	<slot />
+</Content>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,60 +1,15 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
-	import { auth } from '$lib/firebase';
-	import { user as userContext } from '$lib/contexts';
-	import { authState } from 'rxfire/auth';
-	import { setContext } from 'svelte';
-
-	let user = authState(auth);
-
-	setContext(userContext, {
-		getUser: () => $user
-	});
+	import {
+    Grid,
+    Row,
+    Column,
+  } from "carbon-components-svelte";
 </script>
 
-<main>
-	<h1>Hello world!</h1>
-
-	<Counter />
-
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style lang="scss">
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-			'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
-</style>
+<Grid>
+	<Row>
+		<Column>
+			<h1>Welcome</h1>
+		</Column>
+	</Row>
+</Grid>
