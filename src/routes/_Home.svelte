43e0629f24b59a4b4f2487fd6d9563f36c349b4f
Robert Lynch <robertlynch2012@gmail.com>
2021-04-01 18:40:59 -0500

    Do fetch onMount instead of in load

--- src/routes/_Home.svelte
+++ src/routes/_Home.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
 	import type { Recipe } from '$lib/types/recipe';
-	export let recipes: Recipe[];
+	import { onMount } from 'svelte';
+	import { Grid, Row, Column, Tile, Button, Loading } from 'carbon-components-svelte';
+
+	let recipes: Recipe[];
+	onMount(async () => {
+		recipes = await fetch('/user/recipes.json').then((res) => res.json());
+	});
 </script>
 
 <h1>Your Recipes</h1>
 
-{#each recipes as recipe}
-	<h2>{recipe.name}</h2>
-{/each}
+{#if recipes == undefined}
+	<Loading />
+{:else if recipes.length === 0}
+	<Grid>
+		<Row>
+			<Column md={{ span: 2, offset: 3 }}>
+				<Tile>
+					<h3>No recipes!</h3>
+					<p>Create one now?</p>
+				</Tile>
+			</Column>
+		</Row>
+	</Grid>
+{:else}
+	{#each recipes as recipe}
+		<h2>{recipe.name}</h2>
+	{/each}
+{/if}
